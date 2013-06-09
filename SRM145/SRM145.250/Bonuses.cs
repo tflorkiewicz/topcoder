fc025fa8a12b5d5390fe6b3ecd5b1854984f5569
Tom Florkiewicz <tflorkiewicz@gmail.com>
2013-06-09 00:37:56 -0400

    submitted. still doesnt work for test case 2 but no time to fix.

--- SRM145/SRM145.250/Bonuses.cs
+++ SRM145/SRM145.250/Bonuses.cs
@@ -1,27 +1,17 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SRM145._250
-{
-    class Bonuses
+
+    public class Bonuses
     {
         public static void Main()
         {
-            var b = new Bonuses();
-            var r = b.getDivision(new int[]
-            {
-                1,
-                2,
-                3,
-                4,
-                5
-            }
+            Bonuses b = new Bonuses();
+            int[] r = b.getDivision(new int[]
+            {485, 324, 263, 143, 470, 292, 304, 188, 100, 254, 296,
+ 255, 360, 231, 311, 275,  93, 463, 115, 366, 197, 470}
         );
-            foreach (var i in r)
+            foreach (int i in r)
             {
                 Console.WriteLine(i);
             }
@@ -30,44 +20,64 @@ namespace SRM145._250
 
         public int[] getDivision(int[] points)
         {
-            var totalPoints = points.Sum();
-            var result = new int[points.Length];
-            decimal remainder = 0.0m;
-            var idx = 0;
+            int totalPoints = Sum(points);
+            int[] result = new int[points.Length];
+            decimal remainder = 0m;
+            int idx = 0;
 
-            foreach (var i in points)
+            foreach (int i in points)
             {
                 
-                var percent = (int)((i*1.0/totalPoints)*100);
-                decimal x1 = (i*1.0m/totalPoints);
-
-                //remainder += ((decimal)i / totalPoints) * 100 - percent;
-                remainder += x1*100m - (int)(x1*100);
-                
+                int percent = (int)(((i*1m)/totalPoints)*100m);
+                decimal x1 = (i*1m)/totalPoints;
+                remainder += x1*100m - (int)(x1*100m);
                 result[idx] = percent;
                 ++idx;
             }
 
             int[] tmp = new int[points.Length];
-            Array.Copy(result.ToArray(),0, tmp,0,points.Length);
+            Array.Copy(result,0, tmp,0,points.Length);
+            remainder = Math.Round(remainder, 4);
 
-            for (var i = 0; i <= remainder; i++)
+            for (int i = 0; i < remainder; i++)
             {
-                var x = tmp.Max();
+                int? x = Max(tmp);
                 for (int j = 0; j < points.Length; j++)
                 {
                     if (result[j] == x)
                     {
                         result[j]++;
                         tmp[j] = 0;
+                        j = points.Length;
                     }
-                    
                 }
-
             }
-            
             return result;
         }
 
+        private int Sum(IEnumerable<int> a)
+        {
+            int result = 0;
+            foreach (int i in a)
+            {
+                result += i;
+            }
+            return result;
+        }
+
+        private int? Max(int[] a)
+        {
+            if (a.Length == 0)
+                return 0;
+
+            int result = a[0];
+            foreach (int i in a)
+            {
+                if (i > result)
+                    result = i;
+            }
+            return result;
+        }
+
+
     }
-}
