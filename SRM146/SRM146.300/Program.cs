9c9f948b443cd336f6d4dc8ef3409f84037f5f39
Tom Florkiewicz <tflorkiewicz@gmail.com>
2013-06-10 22:30:34 -0400

    SRM 146 - 300 pt problem

---
+++ SRM146/SRM146.300/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SRM146._300
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RectangularGrid r = new RectangularGrid();
+            Console.WriteLine(r.countRectangles(3, 3));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SRM146/SRM146.300/RectangularGrid.cs
@@ -0,0 +1,22 @@
+﻿
+public class RectangularGrid
+{
+    public long countRectangles(int width, int height)
+    {
+        long result = 0;
+        for (int i = 1; i <= width; i++)
+        {
+            for (int j = 1; j <= height; j++)
+            {
+                if (i==j) continue;
+
+                result += (width - i + 1)*(height - j + 1);
+
+            }
+        }
+
+        return result;
+    }
+
+}
+
